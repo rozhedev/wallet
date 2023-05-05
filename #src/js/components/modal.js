3708c03ba46a1bd84bc950ba373dc7f36aa3b54a
rozhedev <digital1479@gmail.com>
2023-05-05 18:01:08 +0300

    Minor stylisation improvements in download-column, faq-accordion, main-rate-item, promoactions-item components

--- #src/js/components/modal.js
+++ #src/js/components/modal.js
@@ -1,7 +1,14 @@
-const promoactionsBtns = document.querySelectorAll(".promoactions-item .btn");
+const promoactionsBtns = document.querySelectorAll(".promoactions-item .btn[type='button']");
+const promoactionsLinks = document.querySelectorAll(".promoactions-item .btn[type='submit']");
 
 promoactionsBtns.forEach((btn) => {
     btn.addEventListener("click", () => {
-        console.log(1);
+        console.log(btn.textContent);
+    })
+})
+
+promoactionsLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+        console.log(link.textContent);
     })
 })
