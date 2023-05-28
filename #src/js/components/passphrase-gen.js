9d0a502f97f322b1fdff45e96745c3991e24f0f8
rozhedev <digital1479@gmail.com>
2023-05-28 11:20:00 +0300

    Transfer passphrase-item & confirm-input as separate component. Minor improvement in pages-layout.scss & passphrase-gen.js

--- #src/js/components/passphrase-gen.js
+++ #src/js/components/passphrase-gen.js
@@ -4,16 +4,23 @@ import { bip39Arr } from "../data/bip39";
 function genPassphrase(inpList, wordArr) {
     let randomIndex = 0;
     let tempArr = [];
-    // let tempIndex = 0;
 
     for (let i = 0; i < inpList.length; i++) {
-        randomIndex = Math.round(Math.random() * wordArr.length);
-
+        randomIndex = getRandomIndex(wordArr);
         tempArr.push(wordArr[randomIndex]);
+        if (
+            i >= 1 &&
+            tempArr[i] == tempArr[i - 1]
+        ) tempArr.push(wordArr[getRandomIndex(wordArr)])
     }
+
     inpList.forEach((inp, i) => {
         inp.value = tempArr[i];
     });
+}
+
+function getRandomIndex(wordArr) {
+    return Math.round(Math.random() * wordArr.length);
 }
 
 genPassphrase(COMMON_NODES.passphraseInputs, bip39Arr);
